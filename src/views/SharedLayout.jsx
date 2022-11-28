ec7ca315e17c3c6aba1809f5fd76dddcb1a96fae
dkobrin <shdwkat999@yahoo.com>
2022-11-28 01:58:39 -0500

    cosmetic changes to title bar, and fixed an image bug for home logo

--- src/views/SharedLayout.jsx
+++ src/views/SharedLayout.jsx
@@ -7,7 +7,7 @@ const SharedLayout = () => {
         <>
             <header>
                 <div className="nav-area">
-                    <NavLink to='/' className='logo'><img className="nav-logo" src='kayla-cares-4-kids-logo.png' alt="Kayla Cares 4 Kids Logo" /></NavLink>
+                    <NavLink to='/' className='logo'><img className="nav-logo" src='/kayla-cares-4-kids-logo.png' alt="Kayla Cares 4 Kids Logo" /></NavLink>
                     <Navbar />
                 </div>
             </header>
